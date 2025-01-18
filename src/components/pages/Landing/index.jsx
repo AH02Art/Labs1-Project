cf889f0ca19cec51f0b6a63aba7ddab9a59f02e4
Alex Handley <smithvillescholarsah@gmail.com>
2025-01-17 18:12:59 -0600

    continueing to add styles

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -6,40 +6,13 @@ import { useNavigate } from 'react-router-dom';
 import { useDownloadData } from '../../../hooks/useDownloadData.js';
 import {decodeBase64} from '../../../utils/decodeBase64.js';
 
+import "./styles.css"; // importing my styles
+
 /**
  * TODO: Ticket 1:
  * Implement structure and styles of the Landing page using Tailwind
  * Implement any button functionality implied by the landing page screenshot example (tickets/examples)
  */
-
-const styles = {
-  landingTop: {
-    backgroundColor: "#666555",
-    padding: "1rem"
-  },
-  landingContent: {
-    margin: "auto"
-  },
-  title: {
-    fontSize: 60,
-    color: "white"
-  },
-  whiteWords: {
-    color: "white",
-    padding: "15px"
-  },
-  blackWords: {
-    fontSize: 24,
-    color: "black",
-    padding: "1.5rem"
-  },
-  images: {
-    float: "center",
-    width: "500px",
-    height: "auto",
-    align: "box"
-  }
-};
 
 export const LandingPage = () => {
   const navigate = useNavigate();
@@ -62,25 +35,25 @@ export const LandingPage = () => {
 
   return (
     <div className='flex-c w-[100vw] secondary-c'>
-      <section style={styles.landingTop}>
-        <h1 style={styles.title}>Asylum Office Grant Rate Tracker</h1>
-        <h3 style={styles.whiteWords}>The Asylum Office Grant Rate Tracker provides asylum seekers, researchers, policymakers, and the public an interactive tool to explore USCIS data on Asylum Office decisions</h3>
+        <div id="title">
+          <h1 id="big-text" className='white-text'>Asylum Office Grant Rate Tracker</h1>
+          <h3 id="small-text" className='white-text'>The Asylum Office Grant Rate Tracker provides asylum seekers, researchers, policymakers, and the public an interactive tool to explore USCIS data on Asylum Office decisions</h3>
+        </div>
+      <section id="section1">
+        <div className="box">
+          <img className="images" src={barGraph} alt="barGraph image"/>
+          <h3 className="image-text">Search Grant Rates By Office</h3>
+        </div>
+        <div className="box">
+          <img className="images" src={pieChart} alt="pieChart image"/>
+          <h3 className="image-text">Search Grant Rates By Nationality</h3>
+        </div>
+        <div className="box">
+          <img className="images" src={lineGraph} alt="lineGraph image"/>
+          <h3 className="image-text">Search Grant Rates Over Time</h3>
+        </div>
       </section>
-      <section style={{display: "block", margin: "auto"}}>
-        <div>
-          <img style={styles.images} src={barGraph} alt="barGraph image" />
-          <h3 style={styles.blackWords}>Search Grant Rates By Office</h3>
-        </div>
-        <div>
-          <img style={styles.images} src={pieChart} alt="pieChart image" />
-          <h3 style={styles.blackWords}>Search Grant Rates By Nationality</h3>
-        </div>
-        <div>
-          <img style={styles.images} src={lineGraph} alt="lineGraph image" />
-          <h3 style={styles.blackWords}>Search Grant Rates Over Time</h3>
-        </div>
-      </section>
-      <div>{'Type this into Canvas: ' + decodeBase64('VGltZTJDb2RlIQ==')}</div>
+      <div className="test">{'Type this into Canvas: ' + decodeBase64('VGltZTJDb2RlIQ==')}</div>
     </div>
   );
 };
