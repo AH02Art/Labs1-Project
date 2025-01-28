eb6fc300e00464753e0c63332fefe42bdc07966a
Alex Handley <smithvillescholarsah@gmail.com>
2025-01-28 02:43:09 -0600

    set up login/logout functionality

--- src/auth/LoggingButtons.jsx
+++ src/auth/LoggingButtons.jsx
@@ -1,18 +1,22 @@
+import { useAuth0 } from "@auth0/auth0-react";
 /**
  * TODO: Ticket 3:
  * Implement authentication and logging functionality using Auth0
  */
 export const LoggingButtons = () => {
-  // TODO: Replace these with Auth0 functionality
-  const isAuthenticated = false;
+  const { isAuthenticated, logout, loginWithRedirect } = useAuth0();
 
   const buttonText = isAuthenticated ? 'Log Out' : 'Log In';
 
   const handleLogging = () => {
     if (isAuthenticated) {
       // TODO: Add Logout functionality here:
+      console.log("Logging out... ");
+      logout({ returnTo: window.location.origin });
     } else {
       // TODO: Add Redirect functionality here:
+      console.log("Logging in... ");
+      loginWithRedirect();
     }
   };
 
--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -1,3 +1,6 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import "../Landing/output.css";
+
 /**
  * TODO: Ticket 3:
  * Implement authentication using Auth0:
@@ -8,15 +11,25 @@
  */
 const Profile = () => {
   // TODO: Replace these with functionality from Auth0
+  const { isAuthenticated, user } = useAuth0();
+  console.log("user data? ==> ", user);
+
   const isLoading = false;
-  const user = true;
+  // const user = true;
 
   if (isLoading || !user) {
     return <div className='text-center p-4'>Loading...</div>;
   }
 
   return (
-    <div>Profile Page</div>
+    <div>
+      <img src={user.picture} alt="User Image"/>
+      <h3>{user.name}</h3>
+      
+      <button onClick={() => console.log("click me!")} className="text-lg">Logout</button>
+      <div>Profile Page</div>
+    </div>
+
   );
 };
 
